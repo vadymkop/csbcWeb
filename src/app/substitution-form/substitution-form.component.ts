ead7eeb2ea5c2feeb15825c32f65018f7d1a3470
VKopachev <kvska@meta.ua>
2019-03-25 10:39:31 +0200

    weekend work

--- src/app/substitution-form/substitution-form.component.ts
+++ src/app/substitution-form/substitution-form.component.ts
@@ -11,8 +11,24 @@ export class SubstitutionFormComponent implements OnInit {
   lessons = ['er4', 'rag2', 'ergea4', '2ar', 'age4'];
   teachers = ['er4', 'rag2', 'ergea4', '2ar', 'age4'];
   audiences = ['er4', 'rag2', 'ergea4', '2ar', 'age4'];
+  subjectNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
   disabledSubject = false;
+
+  activationDate = null;
+  substitutionObj = {
+    activationDate: this.activationDate,
+    subjectNumber: 1,
+    groups: [
+      {
+        name: null,
+        subgroup: {
+          a: true,
+          b: true
+        }
+      }
+    ]
+  };
 
   constructor() {
   }
@@ -20,4 +36,20 @@ export class SubstitutionFormComponent implements OnInit {
   ngOnInit() {
   }
 
+  onAddGroup() {
+    this.substitutionObj.groups.push(
+      {
+        name: null,
+        subgroup: {
+          a: true,
+          b: true
+        }
+      }
+    );
+    console.dir(this.substitutionObj);
+  }
+
+  onDeleteGroup(group: any) {
+    this.substitutionObj.groups.pop();
+  }
 }
